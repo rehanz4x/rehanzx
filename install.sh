91a2a678ca63197fcd97eabf477ecff359985538
Rehan <rehan@gmail.com>
2025-12-09 14:21:05 +0530

    Added automatic pip requirements install

--- install.sh
+++ install.sh
@@ -1,17 +1,33 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo "[*] Updating Termux..."
+clear
+echo "[+] Updating Termux..."
 pkg update -y
 
-echo "[*] Installing Python..."
-pkg install python -y
+echo "[+] Installing Python & Git..."
+pkg install python git -y
 
-echo "[*] Installing rehanzx tool..."
+echo "[+] Installing Python Requirements..."
+pip install requests
+pip install pyfiglet
+pip install rich
 
+echo "[+] Setting permissions..."
+chmod +x rehanzx.py
+chmod +x rehanzx_old.py
+
+echo "[+] Installing launcher & old tool..."
+
+# ✅ Launcher install
 cp rehanzx.py $PREFIX/bin/rehanzx
+
+# ✅ Old tool bhi install hoga
+cp rehanzx_old.py $PREFIX/bin/rehanzx_old.py
+
 chmod +x $PREFIX/bin/rehanzx
+chmod +x $PREFIX/bin/rehanzx_old.py
 
 echo ""
-echo "✅ rehanzx successfully installed!"
-echo "✅ Ab sirf likho: rehanzx"
+echo "[✓] RehanzX Installed Successfully!"
+echo "[✓] Run Tool using: rehanzx"
 echo ""
